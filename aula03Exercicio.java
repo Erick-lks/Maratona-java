b1ea68eee027aeaff32074465b468c55202a7d58
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-03 00:21:16 -0300

    atualização de dados

--- aula03Exercicio.java
+++ aula03Exercicio.java
@@ -9,7 +9,7 @@ public class aula03Exercicio
     public static void main(String[] args)
     {// entrada de dados
         String nome = "Erick Caetano";
-        String endereco = "qn321 conj b lote 1 apt 1005";
+        String endereco = "qn 322 ";
         int salario = 4000;
         String data ="18/06/2023";
         //saida da mensagem
