c40573d6cfaa71b32f07e22e7f04345ad1bb39ad
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-05 00:22:44 -0300

    Aula de Estruturas de repetição

---
+++ Aula06EstruturasdeRepetição01.java
@@ -0,0 +1,21 @@
+public class Aula06EstruturasdeRepetição01 {
+    public static void main(String[] args) {
+        //while , do while ,for
+
+        int contador = 0;
+
+       while (contador <= 10){
+            System.out.println(contador);
+        contador += 1;
+           contador++;
+        }
+        do {
+            System.out.println("vai executar antes de verificar condição" +  ++contador);
+
+        } while (contador < 10);
+
+         for (int i=0 ;i <= 10;i++){
+             System.out.println("for " + i );
+         }
+    }
+}
