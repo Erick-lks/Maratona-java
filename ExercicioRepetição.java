534d7af1f616e67bf424f86349bd4bf82e1f9c2c
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-05 00:34:05 -0300

    Exercicio de repetição

---
+++ ExercicioRepetição.java
@@ -0,0 +1,11 @@
+public class ExercicioRepetição {
+    public static void main(String[] args) {
+        // imprima todos os numeros pares de 2 ate 10000000
+
+
+        for (int i = 0 ; i <= 1000000 ; i++) {
+            if (i % 2 == 0){
+        System.out.println( i);
+        }
+    }
+}}
