9d82c9489041614fc2ecd2f4525a52e9ee973b9c
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-07 01:17:55 -0300

    Exercicio 2 de Estrutura de repetição

---
+++ ExercicioRepeticao02.java
@@ -0,0 +1,19 @@
+public class ExercicioRepeticao02 {
+    public static void main(String[] args) {
+        //Dado um valor de um carro descubra em quantas vezes ele possa ser parcelado
+        // carro condição e que valor da parcela tem que ter >= 1000
+
+
+
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valordaparcela = valorTotal / parcela;
+
+            if (valordaparcela < 1000){
+                       break;
+        }
+                System.out.println("parcela " + parcela + " R$  " + valordaparcela);
+
+        }
+    }
+}
