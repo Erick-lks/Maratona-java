cf0bd606bcddbc2caa001669efab0e2b73e51bc0
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-07 01:56:19 -0300

    Aulas Arrays

---
+++ Aula07Arrays01.java
@@ -0,0 +1,16 @@
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        //para vetores vazios
+        //int, byte, float, double e short = 0
+        //char '/u0000' ou ' '
+        //String null
+        int [] idades = new int[3];
+        idades[0] = 1;
+        idades[1] = 15;
+        idades[2] = 21;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
---
+++ Aula07Arrays02.java
@@ -0,0 +1,13 @@
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+
+      String [] nomes = new String[3];
+      nomes[0] = "Erick" ;
+      nomes[1] = "Joana" ;
+      nomes[2] = "Roberto" ;
+
+        for (int i = 0 ; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+    }}
+
