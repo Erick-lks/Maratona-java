07233b7993e29b0a6bd964a53e0ea5c0a98ef0a7
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-06 23:35:23 -0300

    Estrutura de repetição

---
+++ AuladeRepeticao3.java
@@ -0,0 +1,18 @@
+public class AuladeRepeticao3 {
+    public static void main(String[] args) {
+        //imprima os primeiros 25 numeros de um dado valor, por exemplo 50
+
+    int valormax = 50;
+
+    for (int i = 0; i <= valormax ; i++){
+        if (i > 25)
+            break;
+
+
+        System.out.println( i );
+
+
+
+    }
+
+}}
