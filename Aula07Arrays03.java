3ca6c721ab671d96afffec67964fd476601b3036
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-12 23:24:22 -0300

    Aula de Arrays Multidimencionais

--- Aula07Arrays03.java
+++ Aula07Arrays03.java
@@ -8,8 +8,7 @@ public class Aula07Arrays03 {
 
      //   for (int i = 0; i < numeros3.length; i++){
       //      System.out.println(numeros3[i]);
-        for (int num :numeros3
-             ) {
+        for (int num :numeros3) {
             System.out.println(num);
         }
     }
