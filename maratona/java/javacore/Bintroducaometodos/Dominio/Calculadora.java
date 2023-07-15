642870bca54d98306e997dbcc103f6cac241934f
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-14 22:19:12 -0300

    Aula Metodos

---
+++ maratona/java/javacore/Bintroducaometodos/Dominio/Calculadora.java
@@ -0,0 +1,10 @@
+package maratona.java.javacore.Bintroducaometodos.Dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros(){
+        System.out.println(10 + 10);
+    }
+    public void substraiaDoisNumeros(){
+        System.out.println(15 - 5 );
+    }
+}
