6692a21e626fbcf3db3ba918ceeee1c586beed24
Erick Caetano <erick.caetano1200@gmail.com>
2023-10-07 10:50:08 -0300

    classe polimofirsmo

--- Npolimorfismo/Dominio/Tomate.java
+++ Npolimorfismo/Dominio/Tomate.java
@@ -7,10 +7,26 @@ public class Tomate extends Produto{
     public Tomate(String nome, Double valor) {
         super(nome, valor);
     }
+    private String datadeValidade;
 
     @Override
     public double calcularimposto() {
         System.out.println("Calulando imposto ....");
         return this.valor * IMPOSTO_POR_CENTO;
+    }
+
+
+
+    public Tomate(String nome, Double valor, String datadeValidade) {
+        super(nome, valor);
+        this.datadeValidade = datadeValidade;
+    }
+
+    public String getDatadeValidade() {
+        return datadeValidade;
+    }
+
+    public void setDatadeValidade(String datadeValidade) {
+        this.datadeValidade = datadeValidade;
     }
 }
