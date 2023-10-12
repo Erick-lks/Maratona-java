550b9b6797d59b442788e5afb4b8ad43e6615245
Erick Caetano <erick.caetano1200@gmail.com>
2023-10-12 16:49:57 -0300

    Unchecked Excpetion

---
+++ Oexecption/Runtime/test/RuntimeExcptiontest.java
@@ -0,0 +1,27 @@
+package maratona.java.javacore.Oexecption.Runtime.test;
+
+public interface RuntimeExcptiontest {
+    public static void main(String[] args) {
+
+        System.out.println(divisao(1, 0));
+
+    }
+
+     static int divisao(int a , int b) {
+         if (b == 0){
+             throw new RuntimeException("nao pode ser 0");
+         }
+       /*  try {
+             return a / b;
+
+         } catch (RuntimeException e) {
+             e.printStackTrace();
+         }*/
+         return a/b;
+     }}
+
+
+
+//error de arithmeticException
+//divisao por 0
+
